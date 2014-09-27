fd713890ec85cff0279394896d709116413d0f14
Dmitriy Morozov <dmmo@ciklum.com>
2014-09-27 13:26:55 +0300

    Fixed routing to index.html

--- src/clj/funstructor/core.clj
+++ src/clj/funstructor/core.clj
@@ -22,7 +22,8 @@
       (recur))))
 
 (defroutes app-routes
-  (GET "/" [] (redirect "index.html"))
+  (GET "/cljs" [] (redirect "index-cljs.html"))
+  (GET "/" [] (resource-response "index.html" {:root "public"}))
   (GET "/ws" [] (-> ws-handler
                     (wrap-websocket-handler {:format :json-kw})))
   (route/resources "/")
