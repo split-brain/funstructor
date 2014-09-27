bbf2bea74e6077e6b382e17d6a400a6db71e2d36
mishadoff <mishadoff@gmail.com>
2014-09-27 11:34:19 +0300

    Card links to images

--- src/clj/funstructor/cards.clj
+++ src/clj/funstructor/cards.clj
@@ -10,52 +10,58 @@
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
    :terminal-left-paren
-   {:name "LEFT PAREN"
+   {:name "Left Paren"
     :description
-    "Fill the gap with left parenthesis"
+    "fill the gap on your funstruct with left paren"
     :value :left-paren
     :target :self
-    :type :terminal}
+    :type :terminal
+    :img "terminal_left_paren.svg"}
 
    :terminal-right-paren
-   {:name "RIGHT PAREN"
+   {:name "Right Paren"
     :description
-    "Fill the gap with right parenthesis"
+    "fill the gap on your funstruct with right paren"
     :value :right-paren
     :target :self
-    :type :terminal}
+    :type :terminal
+    :img "terminal_right_paren.svg"}
 
    :terminal-left-square
    {:name "LEFT SQUARE"
     :description
-    "Fill the gap with left square bracket"
+    "fill the gap on your funstruct with left square"
     :value :left-square
     :target :self
-    :type :terminal}
+    :type :terminal
+    :img "terminal_left_square.svg"}
 
    :terminal-right-square
    {:name "RIGHT SQUARE"
     :description
-    "Fill the gap with right square bracket"
+    "fill the gap on your funstruct with right square"
     :value :right-square
     :target :self
-    :type :terminal}
+    :type :terminal
+    :img "terminal_right_square.svg"}
 
    :terminal-id
    {:name "ID"
     :description
-    "Fill the gap with identifier, enter the name"
+    "fill the gap on your funstruct with identifier, according to clojure regexp"
     :value :id
     :target :self
-    :type :terminal}
+    :type :terminal
+    :img "terminal_id.svg"}
 
    :terminal-num
-   {:name "NUM"
+   {:name "Number"
     :description
-    "Fill the gap with number, enter the number"
+    "fill the gap on your funstruct with integer number"
     :value :num
     :target :self
-    :type :terminal}
+    :type :terminal
+    :img "terminal_num.svg"}
 
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
    ;; Mutators
@@ -64,23 +70,26 @@
    :mutator-left-gap
    {:name "Left Gap"
     :description
-    "Add gap to the leftmost position"
+    "add gap to the leftmost position of your funstruct"
     :target :self
-    :type :mutator}
+    :type :mutator
+    :img "mutator_left_gap.svg"}
 
    :mutator-right-gap
    {:name "Right Gap"
     :description
-    "Add gap to the rightmost position"
+    "add gap to the rightmost position of your funstruct"
     :target :self
-    :type :mutator}
+    :type :mutator
+    :img "mutator_right_gap.svg"}
 
    :mutator-position-gap
-   {:name "Position Gap"
+   {:name "Gap"
     :description
-    "Add gap to the specific position, select position"
+    "add gap to the specific position of your funstruct"
     :target :self
-    :type :mutator}
+    :type :mutator
+    :img "mutator_pos_gap.svg"}
    
    
    })
