460c90b7bcc8be5fe4f6c46ffd62996450969d8b
Oleh Palyanytsya <farenheit466@gmail.com>
2014-09-27 12:52:22 +0300

    Added pending checker

--- src/clj/funstructor/commands.clj
+++ src/clj/funstructor/commands.clj
@@ -1,7 +1,7 @@
 (ns funstructor.commands
   (:require [cheshire.core :refer [generate-string parse-string]]
             [funstructor.game-state :refer :all]
-            [clojure.core.async :refer [>! go]]
+            [clojure.core.async :refer [>! <! go go-loop timeout]]
             [funstructor.utils :refer :all]))
 
 (defn encode-command [command]
@@ -15,6 +15,21 @@
     (doseq [channel channels]
       (go
         (>! channel json)))))
+
+(defn pending-checker []
+  (go-loop []
+    (<! (timeout 1000))
+    (when-let [pending-pair (get-pending-pair)]
+      (let [[uuid1 uuid2] pending-pair
+            [channel1 channel2] (map channel-for-uuid pending-pair)]
+        (send-command {:type :start-game
+                       :enemy uuid2}
+                      channel1)
+        (send-command {:type :start-game
+                       :enemy uuid1}
+                      channel2)
+        (apply remove-from-pending pending-pair)))
+    (recur)))
 
 (defmulti handle-command (fn [command channel] (:type command)))
 (defmethod handle-command :game-request [command channel]
--- src/clj/funstructor/game_state.clj
+++ src/clj/funstructor/game_state.clj
@@ -1,7 +1,13 @@
-(ns funstructor.game-state)
+(ns funstructor.game-state
+  (:require
+   funstructor.commands
+   [clojure.set :as set]))
 
 (def game-state (atom {:pending #{}
                        :uuid-channel-map {}}))
+
+(defn init-game-state []
+  (funstructor.commands/pending-checker))
 
 (defn add-channel [channel uuid]
   (swap! game-state update-in [:uuid-channel-map] assoc channel uuid))
@@ -14,3 +20,11 @@
 
 (defn channel-for-uuid [uuid]
   (get-in @game-state [:uuid-channel-map uuid]))
+
+(defn get-pending-pair []
+  (let [pending-players (pending-players)]
+    (when (>= 2 (count pending-players))
+      [(first pending-players) (second pending-players)])))
+
+(defn remove-from-pending [& uuids]
+  (swap! game-state update-in [:pending] set/difference uuids))
