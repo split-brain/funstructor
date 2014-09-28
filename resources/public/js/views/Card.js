af977a58f7801568703b67213d200ec69cc4710d
Maksym Kramarenko <makk@ciklum.com>
2014-09-28 13:40:35 +0300

    UI. Add NUM and ID

--- resources/public/js/views/Card.js
+++ resources/public/js/views/Card.js
@@ -11,9 +11,22 @@ funs.Views.Card = React.createClass({
           data  : this.props.data,
           index : this.props.index 
         };
+        
+        funs.ondragstart(data);
+        event.dataTransfer.setData('text/plain', JSON.stringify(data)); 
+        
+        console.log('DRAG START',event);
+        
+        var crt = event.currentTarget;
 
-        event.dataTransfer.setData('text', JSON.stringify(data)); 
-        console.log('this.dragStart', data);
+        crt.style.opacity = "0.3";
+        event.dataTransfer.setDragImage(crt, 0, 0);
+        
+    },
+    dragEnd: function(event){
+        var crt = event.currentTarget;
+        crt.style.opacity = "1";
+        funs.ondragend();
     },
     render: function() {
         var R = React.DOM;
@@ -26,6 +39,7 @@ funs.Views.Card = React.createClass({
         if(this.props._draggable){
             options.draggable = 'true';
             options.onDragStart = this.dragStart;
+            options.onDragEnd   = this.dragEnd;
         }
 
         var card = R.div(options, R.img({
