8cb1ff69ac1f29943a852523e08770f1a5367e96
Dmitriy Morozov <dmmo@ciklum.com>
2014-09-30 09:52:46 +0300

    removed brepl

--- src/clj/funstructor/brepl.clj
+++
@@ -1,8 +0,0 @@
-(ns funstructor.brepl
-  (:require
-   [cemerick.piggieback]
-   [cljs.repl.browser]))
-
-(defn brepl []
-  (cemerick.piggieback/cljs-repl
-   :repl-env (cljs.repl.browser/repl-env :port 9000)))
