bf4dd3dae786c3b6761a71676dbc8180bb0bc26a
Dmitriy Morozov <dmmo@ciklum.com>
2014-11-23 18:06:08 +0200

    Added Procfile to enable deployment on Heroku.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/funstructor-standalone.jar clojure.main -m funstructor.core
--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies[[org.clojure/clojure "1.7.0-alpha1"]
                 [org.clojure/clojurescript "0.0-2356"]
                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
@@ -15,17 +16,14 @@
                 [reagent "0.4.2"]
                 [com.taoensso/timbre "3.3.1"]
                 [javax.servlet/servlet-api "2.5"]
-
                 [com.cemerick/piggieback "0.1.3"]]
 
+  :uberjar-name "funstructor-standalone.jar"
+  :min-lein-version "2.0.0"
   :plugins [[lein-cljsbuild "1.0.3"]]
-
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-
   :source-paths ["src/clj" "src/cljs"]
-
-  :cljsbuild {
-              :builds [{:id "dev"
+  :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src/cljs"]
                         :compiler
                         {:preamble ["reagent/react.js"]
@@ -48,6 +46,6 @@
                          :closure-warnings {:non-standard-jsdoc :off}}}]}
 
   :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[com.cemerick/austin "0.1.5"]]}}
-
+             :dev {:plugins [[com.cemerick/austin "0.1.5"]]}
+             :production {:env {:production true}}}
   :main funstructor.core)
